9f7eca96e6870548c5536fc02474dae74e8d3b18
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2021-07-12 13:44:27 +0200

    Update compute_dt.jl

--- src/compute_dt.jl
+++ src/compute_dt.jl
@@ -15,12 +15,11 @@ function compute_dt(sqg)
 
     bound1 = maximum(abs.(sqg.u_x .* π ./ dx) .+ abs.(sqg.u_y .* π ./ dy))
     bound1 = 1 / bound1 / 4
-    @show bound1 
 
     # CFL of the hyperviscosity
 
     dx2 = ((dx, dy) ./ pi) .^ 2
-    @show bound2 = 1 / sqg.hv_val * (prod(dx2) / sum(dx2))^(sqg.hv_order ÷ 2)
+    bound2 = 1 / sqg.hv_val * (prod(dx2) / sum(dx2))^(sqg.hv_order ÷ 2)
 
     # Minimum of the CFL
     min(minimum(bound1), bound2)
